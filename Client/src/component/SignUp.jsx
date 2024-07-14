7928e97ca65a153ea724f09cd626693bdba16762
Pawan Kumar <pawan30jul@gmail.com>
2024-07-14 22:39:25 +0530

    signupvalidation

--- Client/src/component/SignUp.jsx
+++ Client/src/component/SignUp.jsx
@@ -1,46 +1,63 @@
 import { useState } from "react";
 import { Link } from "react-router-dom";
 const SignUp = () => {
-    const[inputval,setInputVal] = useState({
-        name:"",
-        email:"",
-        password:"",
+    const [inputval, setInputVal] = useState({
+        name: "",
+        email: "",
+        password: "",
+        error: {
+            name: "",
+            email: "",
+            password: "",
+        },
     });
     console.log(inputval);
 
-    const handleSubmit= (e) =>{
-      const{name,value} = e.target;
-        setInputVal(()=>{
-            return{
-                ...inputval,
-                [name]:value
-            }
-        })
+    const handleSubmit = (e) => {
+        const { name, value } = e.target;
+        setInputVal((prevState) => ({
+            ...prevState,
+            [name]: value,
+        }));
     };
+    
 
     const handleClick = (e) => {
         e.preventDefault();
-        const{name,email,password} = inputval;
-        if(name===""){
-            alert('name not blank');
+        const { name, email, password } = inputval;
+        let errors = {
+            name: "",
+            email: "",
+            password: "",
+        };
+        
+    
+        if (name === "") {
+            errors.name = "Name should not be blank";
+        } else if (name.length < 10) {
+            errors.name = "Name should be at least 10 characters";
         }
-        else if(name<10){
-            alert('name is not not less than 10');
+    
+        if (email === "") {
+            errors.email = "Email should not be empty";
+        } else if (!email.includes("@")) {
+            errors.email = "Email must contain @";
         }
-        else if(email===""){
-            alert('email is not valid');
+    
+        if (password === "") {
+            errors.password = "Password should not be empty";
+        } else if (password.length <= 10) {
+            errors.password = "Password length should be more than 10";
         }
-        else if(!email.includes('@')){
-            alert('@ is include')
-        }
-        else if(password===""){
-            alert('password in not empty');
-        }
-        else{
-            alert('user is successfull');
-            console.log('user is successfull');
-        }
-    }
+    
+        setInputVal((prevState) => ({
+            ...prevState,
+            error: errors,
+        }));
+    
+        return true;
+    };
+    
     return (
         <>
             <section>
@@ -50,14 +67,17 @@ const SignUp = () => {
                             <div className="formWrapper">
                                 <label htmlFor="name" className="labelControl">Name</label>
                                 <input type="text" className="formControl" value={inputval.name} onChange={handleSubmit} name="name" id="name" />
+                                <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.name}</span>
                             </div>
                             <div className="formWrapper">
                                 <label htmlFor="email" className="labelControl">Email</label>
-                                <input type="email" className="formControl" value={inputval.email}  onChange={handleSubmit} name="email" id="email" />
+                                <input type="email" className="formControl" value={inputval.email} onChange={handleSubmit} name="email" id="email" />
+                                <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.email}</span>
                             </div>
                             <div className="formWrapper">
                                 <label htmlFor="password" className="labelControl">Password</label>
-                                <input type="password" className="formControl" value={inputval.password}  onChange={handleSubmit} name="password" id="password" />
+                                <input type="password" className="formControl" value={inputval.password} onChange={handleSubmit} name="password" id="password" />
+                                <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.password}</span>
                             </div>
                             <div>
                                 <button className="btn btn-success mb-2">SignUp</button>
@@ -67,8 +87,8 @@ const SignUp = () => {
                             </div>
                         </form>
                     </div>
-                </div>
-            </section>
+                </div >
+            </section >
         </>
     )
 }
