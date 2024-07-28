7884c1ba117c834147be9224a8ee479fdedcd1be
Pawan Kumar <pawan30jul@gmail.com>
2024-07-28 23:00:02 +0530

    fix some bug

--- Client/src/component/Login.jsx
+++ Client/src/component/Login.jsx
@@ -10,8 +10,6 @@ const Login = () => {
         },
     });
 
-    console.log(inputval);
-
     const handleChange = (e) => {
         const { name, value } = e.target;
         setInputVal((preValue) => {
@@ -28,57 +26,76 @@ const Login = () => {
         let errors = {
             email: "",
             password: "",
-        }
+        };
+
+        let hasErrors = true;
 
         if (email === "") {
             errors.email = "Email is required";
+            hasErrors = false;
         }
         else if (!email.includes('@')) {
             errors.email = "Invalid email";
+            hasErrors = false;
         }
 
         if (password === "") {
             errors.password = "Password is required";
+            hasErrors = false;
         }
         else if (password.length < 6) {
             errors.password = "Password must be at least 6 characters long";
+            hasErrors = false;
         }
 
-        setInputVal((preValue) => {
-            return {
-                ...preValue,
-                error: errors,
-            }
-        });
-        return true;
-    }
-return (
-    <>
-        <section>
-            <div className="container">
-                <div className="loginOuter">
-                    <form action="./action.php" method="post" onSubmit={handleSubmit} className="outerWrapper">
-                        <div className="formWrapper">
-                            <label htmlFor="email" className="labelControl">Email</label>
-                            <input type="email" className="formControl" value={inputval.email} onChange={handleChange} name="email" id="email" />
-                            <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.email}</span>
-                        </div>
-                        <div className="formWrapper">
-                            <label htmlFor="password" className="labelControl">Password</label>
-                            <input type="password" className="formControl" value={inputval.password} onChange={handleChange} name="password" id="password" />
-                            <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.password}</span>
-                        </div>
-                        <div>
-                            <button className="btn btn-primary mb-2">Login</button>
-                        </div>
-                        <div>
-                            <p className="switchaccount mb-0 text-center">If you dont have account Please <Link to="/signup">SignUp</Link></p>
-                        </div>
-                    </form>
+        if (hasErrors) {
+            setInputVal({
+                email: "",
+                password: "",
+                error: {
+                    email: "",
+                    password: ""
+                },
+            });
+            console.log("Successfully Login in!")
+        }
+        else{
+            setInputVal((preValue) => {
+                return {
+                    ...preValue,
+                    error: errors,
+                }
+            });
+        }
+        
+    };
+    return (
+        <>
+            <section>
+                <div className="container">
+                    <div className="loginOuter">
+                        <form action="./action.php" method="post" onSubmit={handleSubmit} className="outerWrapper">
+                            <div className="formWrapper">
+                                <label htmlFor="email" className="labelControl">Email</label>
+                                <input type="email" className="formControl" value={inputval.email} onChange={handleChange} name="email" id="email" />
+                                <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.email}</span>
+                            </div>
+                            <div className="formWrapper">
+                                <label htmlFor="password" className="labelControl">Password</label>
+                                <input type="password" className="formControl" value={inputval.password} onChange={handleChange} name="password" id="password" />
+                                <span style={{ fontSize: "1rem", color: "red" }}>{inputval.error.password}</span>
+                            </div>
+                            <div>
+                                <button className="btn btn-primary mb-2">Login</button>
+                            </div>
+                            <div>
+                                <p className="switchaccount mb-0 text-center">If you dont have account Please <Link to="/signup">SignUp</Link></p>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-            </div>
-        </section>
-    </>
-)
+            </section>
+        </>
+    )
 }
 export default Login;
